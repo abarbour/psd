cb6e9176de1058f7a0adbf72764c81dbf0676e14
Andy Barbour <abarbour@ucsd.edu>
2013-01-22 17:14:27 -0800

    cache clear

--- x_examp/taper.R
+++
@@ -1,8 +0,0 @@
-is.taper(as.taper(1))
-is.taper(as.taper(1:10))
-is.taper(as.taper(matrix(1:10,ncol=1)))
-as.taper(list(x=1:10,y=1:30)) # note dimensions
-as.taper(x<-data.frame(x=1:10,y=10:19))
-as.taper(x, min_taper=3, max_taper=10)
-# class 'character' is in-coercible; raise error
-try(as.taper(c("a","b")))
