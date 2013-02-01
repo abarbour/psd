abec7a6e52d27144dfd8ed3bf8eeea0cd91a4729
Andy Barbour <abarbour@ucsd.edu>
2013-02-01 14:57:35 -0800

    just need to iron out spectrum normalization and finish norm vignette

--- R/func_specprops.R
+++ R/func_specprops.R
@@ -49,6 +49,7 @@
 #' @param f.samp scalar; the sampling frequency (e.g. Hz) of the series the tapers are for
 #' @param n.freq scalar; the number of frequencies of the original spectrum (if \code{NULL} the length of the tapers object is assumed to be the number)
 #' @param p numeric; the coverage probability, bound within \eqn{[0,1)}
+#' @param  db.ci logical; should the uncertainty confidence intervals be returned as decibels?
 #' @param ... additional arguments (unused)
 #' @return A list with the following properties (and names):
 #' \itemize{
@@ -59,7 +60,7 @@
 #' \item{\code{dof}: The number of degrees of freedom.}
 #' \item{\code{bw}: The effective bandwidth of the spectrum.}
 #' }
-spectral_properties <- function(tapvec, f.samp=1, n.freq=NULL, p=0.95, ...) UseMethod("spectral_properties")
+spectral_properties <- function(tapvec, f.samp=1, n.freq=NULL, p=0.95, db.ci=FALSE, ...) UseMethod("spectral_properties")
 #' @rdname spectral_properties
 #' @aliases spectral_properties.spec
 #' @method spectral_properties spec
@@ -76,30 +77,30 @@ spectral_properties.spec <- function(tapvec, ...){
 #' @aliases spectral_properties.tapers
 #' @method spectral_properties tapers
 #' @S3method spectral_properties tapers
-spectral_properties.tapers <- function(tapvec, f.samp=1, n.freq=NULL, p=0.95, ...){
+spectral_properties.tapers <- function(tapvec, f.samp=1, n.freq=NULL, p=0.95, db.ci=FALSE, ...){
   stopifnot(is.tapers(tapvec))
   K <- unclass(tapvec)
   Nyquist <- f.samp/2
   if (is.null(n.freq)) n.freq <- length(tapvec)
-  ## Resolution
-  Resolu <- K * Nyquist / n.freq
   #Var <- 10 / K / 12
-  ## Deg Freedom
+  ## Deg Freedom: PW93 Ch7 343
   Dof <- 2 * K
-  ## Uncertainty
-  ## heuristically tuned to approximate the median distribution of Chi^2 uncertainties
-  StdErr <- 1.32/sqrt(Dof-1) #*2/3)
-  #StdErr <- 1.2 / sqrt(Dof-1)
-  #StdErr <- sqrt(2) /(sqrt(Dof-2)+1)
+  #
+  ##BW <- K / n.freq
+  ##Resolu <- BW * Nyquist
+  #
   ## Bandwidth
   # Walden et al
   # half-width W = (K + 1)/{2(N + 1)}
   # effective bandwidth ~ 2 W (accurate for many spectral windows)
-  BW <- K/n.freq
-  ## CI -- how does it compare to StdErr
-  StdErrChi <- spec_confint(Dof, p)
+  W <- (K+1)/(2*n.freq)
+  BW <- 2 * W
+  ## Resolution
+  Resolu <- 2 * BW
+  ## Uncertainty CI -- how does it compare to StdErr
+  StdErrCI <- spec_confint(Dof, p, as.db=db.ci)
   ##
-  return(data.frame(taper=K, stderr=StdErr, stderr.chi=StdErrChi, resolution=Resolu, dof=Dof, bw=BW))
+  return(data.frame(taper=K, stderr.chi=StdErrCI, resolution=Resolu, dof=Dof, bw=BW))
 }
 
 #' Calculate confidence intervals for a given number of degrees of freedom.
@@ -114,6 +115,7 @@ spectral_properties.tapers <- function(tapvec, f.samp=1, n.freq=NULL, p=0.95, ..
 #' @seealso \code{\link{spectral_properties}}, \code{\link{rlpSpec-package}}, \code{plot.spec}
 #' @param dof numeric; the degrees of freedom
 #' @param p numeric; the coverage probability, bound within \eqn{[0,1)}
+#' @param as.db logical; should the values be returned as decibels?
 #' @return A \code{data.frame} with the following properties (and names):
 #' \itemize{
 #' \item{\code{upper}: The original taper vector.}
@@ -121,12 +123,12 @@ spectral_properties.tapers <- function(tapvec, f.samp=1, n.freq=NULL, p=0.95, ..
 #' \item{\code{median}: The standard error of the spectrum from \code{\link{spec_confint}}.}
 #' }
 #' @keywords properties tapers uncertainty degrees-of-freedom
-spec_confint <- function(dof, p = 0.95) UseMethod("spec_confint")
+spec_confint <- function(dof, p = 0.95, as.db=FALSE) UseMethod("spec_confint")
 #' @rdname spec_confint
 #' @aliases spec_confint.spec
 #' @method spec_confint spec
 #' @S3method spec_confint spec
-spec_confint.spec <- function(dof, p = 0.95){
+spec_confint.spec <- function(dof, p = 0.95, as.db=FALSE){
   stopifnot(is.spec(dof))
   dof <- dof$df
   spec_confint(dof, p)
@@ -135,8 +137,9 @@ spec_confint.spec <- function(dof, p = 0.95){
 #' @aliases spec_confint.tapers
 #' @method spec_confint tapers
 #' @S3method spec_confint tapers
-spec_confint.tapers <- function(dof, p = 0.95){
+spec_confint.tapers <- function(dof, p = 0.95, as.db=FALSE){
   stopifnot(is.tapers(dof))
+  # two degrees of freedom per taper 
   dof <- 2 * unclass(dof)
   spec_confint(dof, p)
 }
@@ -144,7 +147,7 @@ spec_confint.tapers <- function(dof, p = 0.95){
 #' @aliases spec_confint.default
 #' @method spec_confint default
 #' @S3method spec_confint default
-spec_confint.default <- function(dof, p = 0.95) {
+spec_confint.default <- function(dof, p = 0.95, as.db=FALSE) {
   # Mostly from spec.ci, lifted from plot.spec
   if (p < 0 || p >= 1) stop("coverage probability out of range [0,1)")
   ptail <- (1 - p)
@@ -155,11 +158,15 @@ spec_confint.default <- function(dof, p = 0.95) {
   lower.qp <- ptail * pchisq(dof, dof, lower.tail = TRUE)
   ndof <- length(dof)
   # qchisq gives quantile function
-  ci.ul <- 1 / (qchisq(c(upper.qp, lower.qp, med.qp), dof)/sqrt(dof-1)) # dof-1
   # spec.ci calculates with 1/(spec/dof)
+  ci.ul <- 1 / (qchisq(c(upper.qp, lower.qp, med.qp), dof)/dof)
+  # dS = S*ci.ul/dof
+  ## heuristically tuned to approximate the median distribution of Chi^2 uncertainties
+  approx <- 1+1/sqrt(dof-1)
   ci <- data.frame(upper=ci.ul[1:ndof], 
                    lower=ci.ul[(ndof+1):(2*ndof)], 
-                   median=ci.ul[(2*ndof+1):(3*ndof)])
-  #ci$mean <- rowMeans(ci)
+                   median=ci.ul[(2*ndof+1):(3*ndof)],
+                   approx=approx)
+  if (as.db) ci <- dB(ci)
   return(ci)
 }
