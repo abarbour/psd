ef811f70cc125a9ff0b915251f008df17296cd2f
Andrew Barbour <abarbour@usgs.gov>
2015-02-06 16:35:45 -0800

    Streamlined a bit

--- src/parabolic_weights.cpp
+++ src/parabolic_weights.cpp
@@ -5,24 +5,25 @@ using namespace Rcpp; // otherwise add Rcpp::
 //' @export
 // [[Rcpp::export]]
 List parabolic_weights_rcpp(int ntap) {
-    
-    NumericVector kseq = abs( seq_len( ntap ) - 1 );
-    
-    NumericVector ksq = kseq * kseq;
-    int K   = pow(ntap, 1);
-    int K2  = pow(ntap, 2);
-    //int K3  = pow(ntap, 3);
+    //
+    // return quadratic spectral weighting factors for a given number of tapers
+    // Barbour and Parker (2014) Equation 7
+    //
+    NumericVector kseq(ntap), w(ntap);
+    int K = pow(ntap, 1);
+    kseq = abs( seq_len( K ) - 1 );
     
     // orig: w = (tapers^2 - (k-1).^2)*(1.5/(tapers*(tapers-0.25)*(tapers+1)));
-    //NumericVector w = ( K2 - ksq ) * 6 / ( 4 * K3  +  3 * K2  -  K );
-    //NumericVector w = ( K2 - ksq ) / ( K * (4 * K  -  1) * (K  +  1) );
-    NumericVector w = ( K2 - ksq ) * ( 1.5 / ( K * (K  -  0.25) * (K  +  1) ) );
+    //   or: w = ( K2 - ksq ) * 6 / ( 4 * K3  +  3 * K2  -  K );
+    //   or: w = ( K2 - ksq ) / ( K * (4 * K  -  1) * (K  +  1) );
+    
+    w = ( K * K  -  kseq * kseq ) * ( 1.5 / ( K * (K  -  0.25) * (K  +  1) ) );
     
     List Weights = List::create(
-                              Named("ntap") = K,
-                              Named("taper_seq") = kseq+1,
-                              Named("taper_weights") = w
-                              );
+                                Named("ntap") = K,
+                                Named("taper_seq") = kseq + 1,
+                                Named("taper_weights") = w
+                                );
     
     return Weights;
 }
